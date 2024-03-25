d27b51742fbb732874bb200566b68aa12089befe
051hui <sheekang30302@gmail.com>
2024-03-25 18:26:50 +0900

    feat: organize

---
+++ index.js
@@ -0,0 +1,6 @@
+document.getElementById("close").addEventListener("click", function () {
+  document.getElementById("alert").style.display = "none";
+});
+function 알림창열기(구멍) {
+  document.getElementById("alert").style.display = 구멍;
+}
