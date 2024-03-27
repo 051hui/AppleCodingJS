85be331f5366a0b6f8a7d698a8eef4a3c44c9dde
Hui Kang <sheekang30302@gmail.com>
2024-03-27 22:28:23 +0900

    feat: form using if/else (#9)

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 document.querySelector("#login").addEventListener("click", function () {
   document.querySelector(".black-bg").classList.add("show-modal");
 });
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  if (
+    document.getElementById("email").value == "" ||
+    document.getElementById("password").value == ""
+  ) {
+    alert("공란 안돼요");
+    e.preventDefault(); //폼 전송 막음
+  } else if (document.getElementById("password").value.length < 6) {
+    alert("비밀번호 6자 이상이요");
+  }
+});
