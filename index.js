203205d1aceaec42e7d6a0e560454dd5f3d432e0
Hui Kang <sheekang30302@gmail.com>
2024-04-09 17:41:23 +0900

    hw function added (#10)

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-document.querySelector("#login").addEventListener("click", function () {
-  document.querySelector(".black-bg").classList.add("show-modal");
-});
+const login = document.querySelector("#login");
+const form = document.querySelector("form");
 
-document.querySelector("form").addEventListener("submit", function (e) {
+login.addEventListener("click", showModal());
+
+function showModal() {
+  document.querySelector(".black-bg").classList.add("show-modal");
+}
+
+form.addEventListener("submit", function (e) {
   if (
     document.getElementById("email").value == "" ||
     document.getElementById("password").value == ""
@@ -13,3 +18,26 @@ document.querySelector("form").addEventListener("submit", function (e) {
     alert("비밀번호 6자 이상이요");
   }
 });
+
+function game(n) {
+  if (n % 9 == 0) {
+    console.log("박수를 치세요*2");
+  } else if (n % 3 == 0) {
+    console.log("박수를 치세요");
+  } else {
+    console.log("통과");
+  }
+}
+
+function 합격했냐(a, b) {
+  if (a < 40 || b < 40) {
+    console.log("불합격");
+  } else if (a + b >= 120) {
+    console.log("합격");
+  } else {
+    console.log("불합격");
+  }
+}
+
+//game(9);
+//합격했냐(50, 50);
