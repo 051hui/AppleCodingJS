8ce0e5f18fe15bc7f010dd2ca266a88f038bfe4a
051hui <sheekang30302@gmail.com>
2024-05-03 01:57:29 +0900

    regular expression

--- index.js
+++ index.js
@@ -14,6 +14,26 @@ form.addEventListener("submit", function (e) {
     e.preventDefault(); //폼 전송 막음
   } else if (document.getElementById("password").value.length < 6) {
     alert("비밀번호 6자 이상이요");
+    e.preventDefault();
+  }
+});
+
+document.querySelector("form").addEventListener("submit", function (e) {
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  if (email == "") {
+    alert("아이디입력안함");
+    e.preventDefault();
+  } else if (/\S+@\S+\.\S+/.test(email) == false) {
+    alert("이메일형식아님");
+    e.preventDefault();
+  }
+  if (password == "") {
+    alert("비밀번호입력안함");
+    e.preventDefault();
+  } else if (password.value.length <= 6) {
+    alert("비밀번호7자리이상입력");
+    e.preventDefault();
   }
 });
 
